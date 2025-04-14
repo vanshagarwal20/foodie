1f54da78095f88413843c3422756df305ecc5324
Vansh Agarwal <agarwalvansh3329@gmail.com>
2025-04-14 16:39:45 +0530

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home/home'
-import Cart from './pages/Cart/cart'
-import Placeorder from './pages/Placeorder/placeorder'
+import Home from './pages/Home/Home'
+import Cart from './pages/Cart/Cart'
+import Placeorder from './pages/Placeorder/Placeorder'
 import Footer from './components/Footer/Footer'
 import LoginPopup from './components/LoginPopup/LoginPopup'
 
